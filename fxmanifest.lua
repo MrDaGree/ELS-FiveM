596517286e115992927d0f9e0d270c3ba3cc0501
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 18:01:46 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,16 +6,16 @@ description 'ELS FiveM'
 version '1.7.0'
 
 client_script {
-     'vcf.lua',
-     'config.lua',
-     'client/**/*.lua'
+    'vcf.lua',
+    'config.lua',
+    'client/**/*.lua'
 }
 
 server_script {
-     'vcf.lua',
-     'config.lua',
-     'server/server.lua',
-     'server/xml.lua'
+    'vcf.lua',
+    'config.lua',
+    'server/server.lua',
+    'server/xml.lua'
 }
 
 shared_script 'shared/*.lua'
