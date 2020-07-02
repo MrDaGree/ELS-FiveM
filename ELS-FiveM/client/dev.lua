935df0fe87e250c43cb9cd18de112e41c4d11963
Zeemah <zeemah@protonmail.ch>
2020-07-02 23:01:15 +0100

    chore: remove dev files

--- ELS-FiveM/client/dev.lua
+++
@@ -1,7 +0,0 @@
-local orig = _G.Citizen.Trace
-_G.Citizen.Trace = function(data)
-    orig(data)
-    if string.match(data, "SCRIPT ERROR") or string.match(data, "error") then
-        TriggerServerEvent("SentryIO:Error", data:gsub(":(.*)", ""):gsub("%^%d", ""), data:gsub("%^%d", ""))
-    end
-end
