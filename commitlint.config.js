41dece1ab59bf47d54355dd7e2dda0448a67c95b
zeemah <zeemah@protonmail.ch>
2020-08-12 00:31:37 +0100

    fix: oops

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,43 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    parserPreset: 'conventional-changelog-conventionalcommits',
-    rules: {
-        'body-leading-blank': [1, 'always'],
-        'body-max-line-length': [2, 'always', 100],
-        'footer-leading-blank': [1, 'always'],
-        'footer-max-line-length': [2, 'always', 100],
-        'header-max-length': [2, 'always', 100],
-        'scope-case': [2, 'always', 'lower-case'],
-        'subject-case': [
-            2,
-            'never',
-            ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-        ],
-        'subject-empty': [2, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
-        'type-case': [2, 'always', 'lower-case'],
-        'type-empty': [2, 'never'],
-        'type-enum': [
-            2,
-            'always',
-            [
-                'build',
-                'chore',
-                'ci',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test',
-                'git'
-            ]
-        ]
-    }
-};
-=======
 	parserPreset: 'conventional-changelog-conventionalcommits',
 	rules: {
 		'body-leading-blank': [1, 'always'],
@@ -75,4 +36,3 @@ module.exports = {
 		]
 	}
 };
->>>>>>> 4bf59b05b9094b162367fcbd54ac213f00977b62
